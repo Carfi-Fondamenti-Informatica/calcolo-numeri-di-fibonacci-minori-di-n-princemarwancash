cf8ddbf07a83753c978311da7c9a3e8f40233b59
Marwan Keshik <91651940+princemarwancash@users.noreply.github.com>
2021-11-13 15:19:58 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
-
-int main() {
-   return 0;
+int main(){
+    int num1=0;
+    int num2=1;
+    int risultato=0;
+    int i=0;
+    int n=0;
+    cin >> n ;
+    cout << 1 << endl;
+    for (i = 0; i<=n ; i++) {
+        risultato = num1 + num2;
+        cout << risultato << endl;
+        num1 = num2;
+        num2 = risultato;
+    }
+    return 0;
 }
